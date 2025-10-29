e204ede39979d8668b0281065c1d2a825df8611e
Abu-Ubaidah <101874033+Abu-Ubaidah@users.noreply.github.com>
2025-10-29 11:55:11 +0500

    modified code

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-app.get("/google",(req,res)=>{
+app.get("/",(req,res)=>{
+    res.send("hello test")
+});app.get("/google",(req,res)=>{
     res.send("hello tgere")
 });
 
